bb3461cfaf6fabc8b7b114d4f988bd98abb82d73
Jonas Ohland <jonas.ohland@gmail.com>
2020-06-04 15:44:44 +0200

    newest shgen version

--- Formula/shgen.rb
+++ Formula/shgen.rb
@@ -1,16 +1,16 @@
 class Shgen < Formula
     desc "Parse ini file in CMake"
-    url "https://github.com/jonasohland/shgen/tarball/0.0.4"
-    sha256 "349c9c2dec2f81133f59193e12d0740bd9009f626d854bb5268abe15c301c713"
+    url "https://github.com/jonasohland/shgen/tarball/0.0.6"
+    sha256 "8a222aca97cecb82f3bc59db1e98526a3f20ac120cb1abb9dea5c26b3b4c55e4"
 
     depends_on "cmake"
 
     def install
         system "curl -L https://github.com/catchorg/Clara/tarball/v1.1.5 -o clara.tar"
         system "tar -xf clara.tar"
-        system "rm -rf dep/Clara" 
+        system "rm -rf dep/Clara"
         system "mv catchorg-* dep/Clara"
-        system "cmake", ".", *std_cmake_args
+        system "cmake", ".", *std_cmake_args, "-Dshgen_version=0.0.6"
         system "cmake", "--build", ".", "--target", "install"
     end
 end
